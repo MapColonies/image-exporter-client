5ecb4e1a2af0d8c9cf3977235aae5344f5051511
alebinson <newdarkvirus@gmail.com>
2020-09-29 16:27:20 +0300

    fix(code): cleanup

--- src/exporter/components/export-table/cell-renderer/progress.cell-renderer.tsx
+++ src/exporter/components/export-table/cell-renderer/progress.cell-renderer.tsx
@@ -21,9 +21,6 @@ export const ProgressRenderer: React.FC<ICellRendererParams> = (
     return value > 1 ? `${value}%` : `${value * ONE_HUNDRED}%`;
   }
 
-  // return (
-  //   props.data.progress
-  // );
   return (
     <Box style={{display: 'flex', height: '40px', alignItems: 'center'}}>
       <Typography use="body1" className={'rendererLabel'}>{getPercentageText()}</Typography>
@@ -32,49 +29,3 @@ export const ProgressRenderer: React.FC<ICellRendererParams> = (
   );
 
 };
-
-// export class ProgressRenderer extends React.Component<any,any> {
-//   constructor(props:any) {
-//     super(props);
-
-//     this.state = {
-//       value: props.value,
-//     };
-//   }
-
-//   formatValueToCurrency(currency:any, value:any) {
-//     return `${currency}${value.toFixed(2)}`;
-//   }
-
-//   getProgressValue():number {
-//     return this.state.value > 1 ? this.state.value/100 : this.state.value;
-//   }
-//   // noinspection JSUnusedGlobalSymbols
-//   refresh(params:any) {
-//     if (params.value !== this.state.value) {
-//       this.setState({
-//         value: params.value,
-//       });
-//     }
-//     return true;
-//   }
-
-//   render() {
-//     console.log('Render PROGRESS');
-//     // return <span>{this.formatValueToCurrency('EUR', this.state.value)}</span>;
-//     return (
-//       <div style={{display: 'flex', height: '40px', alignItems: 'center'}}>
-//         <span style={{
-//           fontSize: '12px',
-//           position: 'absolute',
-//           left: '50%',
-//           top: '-12px'
-//         }}>
-//           80%
-//         </span>
-//         <Typography use="body1" className={classes.infoLabel}></Typography>
-//         <LinearProgress progress={this.getProgressValue()} />
-//       </div>
-//     );
-//   }
-// }
