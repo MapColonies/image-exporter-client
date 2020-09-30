066c1bbfec9dd15baa4769396160bff213b1e585
alebinson <newdarkvirus@gmail.com>
2020-09-30 22:42:45 +0300

    fix(unit-test): tests wrapped by suite

--- src/exporter/components/map-container/polygon-selection-ui.spec.tsx
+++ src/exporter/components/map-container/polygon-selection-ui.spec.tsx
@@ -35,134 +35,137 @@ afterEach(() => {
   resetDraw.mockClear();
 });
 
-it('renders correctly', () => {
-  const wrapper = shallow(
-    <PolygonSelectionUi
-      isSelectionEnabled={false}
-      onCancelDraw={() => {}}
-      onReset={() => {}}
-      onStartDraw={() => {}}
-      onPolygonUpdate={(polygon) => {}}
-    />
-  );
-
-  expect(wrapper).toMatchSnapshot();
-});
-
-it('opens the menu with drawing options on open menu button click', () => {
-  const wrapper = shallow(
-    <PolygonSelectionUi
-      onStartDraw={startDraw}
-      onCancelDraw={cancelDraw}
-      onReset={resetDraw}
-      isSelectionEnabled={false}
-      onPolygonUpdate={(polygon) => {}}
-    />
-  );
-
-  expect(wrapper.find(Menu).prop('open')).toBe(false); 
-
-  wrapper.find(Button).simulate('click', { currentTarget: {} });
-
-  expect(wrapper.find(Menu).prop('open')).toBe(true);
-});
-
-it('Box drawing menu items call start draw with correct params on click and closes the menu', () => {
-  const wrapper = shallow(
-    <PolygonSelectionUi
-      onStartDraw={startDraw}
-      onCancelDraw={cancelDraw}
-      onReset={resetDraw}
-      isSelectionEnabled={false}
-      onPolygonUpdate={(polygon) => {}}
-    />
-  );
-
-  const openMenuButton = wrapper.find(Button);
- 
-  openMenuButton.simulate('click', { currentTarget: {} });
-
-  wrapper
-    .findWhere((n) => {
-      return n.type() === MenuItem && 
-            n.prop('children').props['id'] === 'polygon-selection.box-menu_option.text';
-    })
-    .simulate('click');
-
-  expect(wrapper.find(Menu).prop('open')).toBe(false);
-  expect(startDraw).toHaveBeenCalledWith(DrawType.BOX);
-  expect(startDraw).toHaveBeenCalledTimes(1);
-});
-
-it('Custom defined Bbox drawing menu items opens BBox definition dialog on click and closes the menu', () => {
-  const wrapper = shallow(
-    <PolygonSelectionUi
-      onStartDraw={startDraw}
-      onCancelDraw={cancelDraw}
-      onReset={resetDraw}
-      isSelectionEnabled={false}
-      onPolygonUpdate={(polygon) => {}}
-    />
-  );
-
-  const openMenuButton = wrapper.find(Button);
-  openMenuButton.simulate('click', { currentTarget: {} });
-
-  wrapper
-    .findWhere((n) => {
-      return n.type() === MenuItem && 
-            n.prop('children').props['id'] === 'polygon-selection.box_coorinate-menu_option.text';
-    })
-    .simulate('click');
-
-  // expect(wrapper.find(Menu).prop('open')).toBe(false);
-  expect(wrapper.find(DialogBBox).prop('isOpen')).toBe(true);
-});
-
-
-it('clicking the clear menu item calls onreset and closes the menu', () => {
-  const wrapper = shallow(
-    <PolygonSelectionUi
-      onStartDraw={startDraw}
-      onCancelDraw={cancelDraw}
-      onReset={resetDraw}
-      isSelectionEnabled={false}
-      onPolygonUpdate={(polygon) => {}}
-    />
-  );
-
-  const openMenuButton = wrapper.find(Button);
-  openMenuButton.simulate('click', { currentTarget: {} });
-
-  wrapper
-    .findWhere((n) => {
-      return n.type() === MenuItem && 
-            n.prop('children').props['id'] === 'polygon-selection.clear-menu_option.text';
-    })
-    .simulate('click');
-
-  expect(wrapper.find(Menu).prop('open')).toBe(false);
-  expect(resetDraw).toHaveBeenCalledTimes(1);
-});
-
-it('Cancel draw is shown when IsSelectionEnabled is true, and clicking on the button calls onCancelDraw', () => {
-  const wrapper = shallow(
-    <PolygonSelectionUi
-      onStartDraw={startDraw}
-      onCancelDraw={cancelDraw}
-      onReset={resetDraw}
-      isSelectionEnabled={true}
-      onPolygonUpdate={(polygon) => {}}
-    />
-  );
-
-  const button = wrapper
-  .findWhere((n) => {
-    return n.type() === Button && 
-          n.prop('children').props['id'] === 'polygon-selection.cancel-btn.text';
+describe('Polygon Selection component', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(
+      <PolygonSelectionUi
+        isSelectionEnabled={false}
+        onCancelDraw={() => {}}
+        onReset={() => {}}
+        onStartDraw={() => {}}
+        onPolygonUpdate={(polygon) => {}}
+      />
+    );
+  
+    expect(wrapper).toMatchSnapshot();
   });
-
-  button.simulate('click');
-
-  expect(cancelDraw).toHaveBeenCalledTimes(1);
+  
+  it('opens the menu with drawing options on open menu button click', () => {
+    const wrapper = shallow(
+      <PolygonSelectionUi
+        onStartDraw={startDraw}
+        onCancelDraw={cancelDraw}
+        onReset={resetDraw}
+        isSelectionEnabled={false}
+        onPolygonUpdate={(polygon) => {}}
+      />
+    );
+  
+    expect(wrapper.find(Menu).prop('open')).toBe(false); 
+  
+    wrapper.find(Button).simulate('click', { currentTarget: {} });
+  
+    expect(wrapper.find(Menu).prop('open')).toBe(true);
+  });
+  
+  it('Box drawing menu items call start draw with correct params on click and closes the menu', () => {
+    const wrapper = shallow(
+      <PolygonSelectionUi
+        onStartDraw={startDraw}
+        onCancelDraw={cancelDraw}
+        onReset={resetDraw}
+        isSelectionEnabled={false}
+        onPolygonUpdate={(polygon) => {}}
+      />
+    );
+  
+    const openMenuButton = wrapper.find(Button);
+   
+    openMenuButton.simulate('click', { currentTarget: {} });
+  
+    wrapper
+      .findWhere((n) => {
+        return n.type() === MenuItem && 
+              n.prop('children').props['id'] === 'polygon-selection.box-menu_option.text';
+      })
+      .simulate('click');
+  
+    expect(wrapper.find(Menu).prop('open')).toBe(false);
+    expect(startDraw).toHaveBeenCalledWith(DrawType.BOX);
+    expect(startDraw).toHaveBeenCalledTimes(1);
+  });
+  
+  it('Custom defined Bbox drawing menu items opens BBox definition dialog on click and closes the menu', () => {
+    const wrapper = shallow(
+      <PolygonSelectionUi
+        onStartDraw={startDraw}
+        onCancelDraw={cancelDraw}
+        onReset={resetDraw}
+        isSelectionEnabled={false}
+        onPolygonUpdate={(polygon) => {}}
+      />
+    );
+  
+    const openMenuButton = wrapper.find(Button);
+    openMenuButton.simulate('click', { currentTarget: {} });
+  
+    wrapper
+      .findWhere((n) => {
+        return n.type() === MenuItem && 
+              n.prop('children').props['id'] === 'polygon-selection.box_coorinate-menu_option.text';
+      })
+      .simulate('click');
+  
+    // expect(wrapper.find(Menu).prop('open')).toBe(false);
+    expect(wrapper.find(DialogBBox).prop('isOpen')).toBe(true);
+  });
+  
+  
+  it('clicking the clear menu item calls onreset and closes the menu', () => {
+    const wrapper = shallow(
+      <PolygonSelectionUi
+        onStartDraw={startDraw}
+        onCancelDraw={cancelDraw}
+        onReset={resetDraw}
+        isSelectionEnabled={false}
+        onPolygonUpdate={(polygon) => {}}
+      />
+    );
+  
+    const openMenuButton = wrapper.find(Button);
+    openMenuButton.simulate('click', { currentTarget: {} });
+  
+    wrapper
+      .findWhere((n) => {
+        return n.type() === MenuItem && 
+              n.prop('children').props['id'] === 'polygon-selection.clear-menu_option.text';
+      })
+      .simulate('click');
+  
+    expect(wrapper.find(Menu).prop('open')).toBe(false);
+    expect(resetDraw).toHaveBeenCalledTimes(1);
+  });
+  
+  it('Cancel draw is shown when IsSelectionEnabled is true, and clicking on the button calls onCancelDraw', () => {
+    const wrapper = shallow(
+      <PolygonSelectionUi
+        onStartDraw={startDraw}
+        onCancelDraw={cancelDraw}
+        onReset={resetDraw}
+        isSelectionEnabled={true}
+        onPolygonUpdate={(polygon) => {}}
+      />
+    );
+  
+    const button = wrapper
+    .findWhere((n) => {
+      return n.type() === Button && 
+            n.prop('children').props['id'] === 'polygon-selection.cancel-btn.text';
+    });
+  
+    button.simulate('click');
+  
+    expect(cancelDraw).toHaveBeenCalledTimes(1);
+  });
 });
+
