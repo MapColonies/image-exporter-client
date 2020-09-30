41371c0651888eaf4d0e2f0f01ac380d86758f98
alebinson <newdarkvirus@gmail.com>
2020-09-30 22:36:30 +0300

    fix(eslint): disable few rules in spec file

--- src/exporter/components/map-container/polygon-selection-ui.spec.tsx
+++ src/exporter/components/map-container/polygon-selection-ui.spec.tsx
@@ -8,18 +8,22 @@ import { DialogBBox } from './dialog-bbox';
 // Enzyme doesn’t work properly with hooks in general, especially for `shallow` so this is the way to mock `react-intl` module.
 // Enspired by https://github.com/formatjs/formatjs/issues/1477
 jest.mock('react-intl', () => {
+  /* eslint-disable */
   const reactIntl = require.requireActual('react-intl');
   const MESSAGES =  require.requireActual('../../../common/i18n');
   const intl = reactIntl.createIntl({
     locale: 'en',
     messages: MESSAGES.default['en'],
-  })
+  });
 
   return {
     ...reactIntl,
     useIntl: () => intl,
-  }
+  };
+  /* eslint-enable */
 });
+
+/* eslint-disable @typescript-eslint/no-empty-function, @typescript-eslint/no-unsafe-member-access */
 
 const startDraw = jest.fn();
 const cancelDraw = jest.fn();
