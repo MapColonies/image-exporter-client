88ece2c345125ebdf7efa50d72041bea2e3a9640
shimoncohen <33935191+shimoncohen@users.noreply.github.com>
2020-11-01 11:49:25 +0200

    fix(dockerfile): run confd in docker entrypoint (#77)
    * fix(dockerfile): run confd in docker entrypoint
    
    * chore(dockerfile): remove unnecessary copy of package files

--- run.sh
+++ run.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-xhost +local:root
-
 # Default values of arguments
 ENTERYPOINT=0
 
 # Loop through arguments and process them
 # Taken from: https://pretzelhands.com/posts/command-line-flags
-for arg in "$@"
-do
+for arg in "$@"; do
     case $arg in
-        --entrypoint)
+    --entrypoint)
         ENTERYPOINT=1
         shift # Remove
         ;;
@@ -18,7 +15,14 @@ do
 done
 
 if [ $ENTERYPOINT -eq 1 ]; then
-	docker run --rm -it --entrypoint /bin/sh -p 3000:80 --name exporterclientcontainer exporterclient:v1
+    docker run --rm -it \
+        --entrypoint /bin/sh \
+        -p 3000:80 \
+        --name exporterclientcontainer \
+        exporterclient:v1
 else
-	docker run --rm -p 3000:80 --name exporterclientcontainer exporterclient:v1 &
+    docker run -d --rm \
+        -p 3000:80 \
+        --name exporterclientcontainer \
+        exporterclient:v1
 fi
