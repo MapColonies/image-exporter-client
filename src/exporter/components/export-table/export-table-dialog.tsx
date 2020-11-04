02bf04dd9520b4449fce7526ed22ef2973e26c18
Alex Lebinson <newdarkvirus@gmail.com>
2020-11-04 14:52:26 +0200

    feat(data-polling): exported packages polling (#88)
    Co-authored-by: ALEXANDER LEVINSON <ALEXLEB@rf.local>

--- src/exporter/components/export-table/export-table-dialog.tsx
+++ src/exporter/components/export-table/export-table-dialog.tsx
@@ -3,9 +3,11 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import moment from 'moment';
 import { observer } from 'mobx-react-lite';
 import { AgGridReact } from 'ag-grid-react';
-import { 
-  ColDef, 
+import {
+  ColDef,
   ICellRendererParams,
+  GridReadyEvent,
+  GridApi,
 } from 'ag-grid-community';
 import {
   Dialog,
@@ -16,6 +18,7 @@ import {
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import { Box } from '@map-colonies/react-components';
+import EXPORTER_CONFIG from '../../../common/config';
 import { useStore } from '../../models/rootStore';
 import { IExportTaskStatus, IBbox } from '../../models/exportTaskStatus';
 import { ProgressRenderer } from './cell-renderer/progress.cell-renderer';
@@ -27,150 +30,245 @@ interface ExportSatusTableDialogProps {
   onSetOpen: (open: boolean) => void;
 }
 
-export const ExportSatusTableDialog: React.FC<ExportSatusTableDialogProps> = observer((
-  props
-) => {
-  const { exporterStore } = useStore();
-  const { isOpen, onSetOpen } = props;
-  const intl = useIntl();
-  const [colDef, setColDef] = useState<ColDef[]>([]);
-  const [rowData, setRowData] = useState([]);
-  const pageSize = 10;
+export const ExportSatusTableDialog: React.FC<ExportSatusTableDialogProps> = observer(
+  (props) => {
+    const START_CYCLE_ITTERACTION = 0;
+    const { exporterStore } = useStore();
+    const { isOpen, onSetOpen } = props;
+    const intl = useIntl();
+    const [gridApi, setGridApi] = useState<GridApi>();
+    const [colDef, setColDef] = useState<ColDef[]>([]);
+    const [rowData, setRowData] = useState([]);
+    const [pollingCycle, setPollingCycle] = useState(START_CYCLE_ITTERACTION);
+    const pageSize = 10;
 
-  const handleClose = (): void => {
-    onSetOpen(false);
-  };
+    const handleClose = (): void => {
+      onSetOpen(false);
+    };
 
-  const renderDate = (date : Date | undefined): string => {
-    return date ? moment(new Date(date.toLocaleString())).format('DD/MM/YYYY HH:mm') : "-";
-  }
+    const renderDate = (date: Date | undefined): string => {
+      return date
+        ? moment(new Date(date.toLocaleString())).format('DD/MM/YYYY HH:mm')
+        : '-';
+    };
 
-  const renderBbox = (bbox : IBbox | undefined) : string => {
-    return bbox ? `Top right: ${bbox.topRight.lat}, ${bbox.topRight.lon}, Bottom left: ${bbox.bottomLeft.lat}, ${bbox.bottomLeft.lon}` : '';
-  }
+    const renderBbox = (bbox: IBbox | undefined): string => {
+      return bbox
+        ? `Top right: ${bbox.topRight.lat}, ${bbox.topRight.lon}, Bottom left: ${bbox.bottomLeft.lat}, ${bbox.bottomLeft.lon}`
+        : '';
+    };
 
-  useEffect(()=>{
-    // File name | Est Size | Status | (URI) Link to download | Date | Progress
-    setColDef([
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.fileName.text' }),
-        width: 200,
-        field: 'fileName',
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.status.text' }),
-        width: 120,
-        field: 'status',
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.link.text' }),
-        width: 120,
-        field: 'link',
-        cellRenderer: 'linkRenderer',
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.creationDate.text' }),
-        width: 170,
-        field: 'creationDate',
-        cellRenderer: (props : ICellRendererParams) : string => {
-          const data = props.data as IExportTaskStatus;
-          return renderDate(data.creationDate);
+    const onGridReady = (params: GridReadyEvent): void => {
+      setGridApi(params.api);
+    };
+
+    useEffect(() => {
+      // File name | Est Size | Status | (URI) Link to download | Date | Progress
+      setColDef([
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.fileName.text',
+          }),
+          width: 200,
+          field: 'fileName',
+          suppressMovable: true,
         },
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.lastUpdateTime.text' }),
-        width: 170,
-        field: 'lastUpdateTime',
-        cellRenderer: (props : ICellRendererParams) : string => {
-          const data = props.data as IExportTaskStatus;
-          return renderDate(data.lastUpdateTime);
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.status.text',
+          }),
+          width: 120,
+          field: 'status',
+          suppressMovable: true,
         },
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.progress.text' }),
-        width: 120,
-        field: 'progress',
-        cellRenderer: 'progressRenderer',
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.sizeEst.text' }),
-        width: 120,
-        field: 'sizeEst',
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.tilesEst.text' }),
-        width: 100,
-        field: 'tilesEst',
-        suppressMovable: true,
-      },
-      {
-        headerName: intl.formatMessage({ id: 'export-table.table-column-header.bbox.text' }),
-        width: 800,
-        field: 'bbox',
-        cellRenderer: (props : ICellRendererParams) : string => {
-          const data = props.data as IExportTaskStatus;
-          const bbox = data.bbox;
-          return renderBbox(bbox);
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.link.text',
+          }),
+          width: 120,
+          field: 'link',
+          cellRenderer: 'linkRenderer',
+          suppressMovable: true,
         },
-        suppressMovable: true,
-      },
-    ]);
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.creationDate.text',
+          }),
+          width: 170,
+          field: 'creationDate',
+          cellRenderer: (props: ICellRendererParams): string => {
+            const data = props.data as IExportTaskStatus;
+            return renderDate(data.creationDate);
+          },
+          suppressMovable: true,
+        },
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.lastUpdateTime.text',
+          }),
+          width: 170,
+          field: 'lastUpdateTime',
+          cellRenderer: (props: ICellRendererParams): string => {
+            const data = props.data as IExportTaskStatus;
+            return renderDate(data.lastUpdateTime);
+          },
+          suppressMovable: true,
+        },
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.progress.text',
+          }),
+          width: 120,
+          field: 'progress',
+          cellRenderer: 'progressRenderer',
+          suppressMovable: true,
+        },
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.sizeEst.text',
+          }),
+          width: 120,
+          field: 'sizeEst',
+          suppressMovable: true,
+        },
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.tilesEst.text',
+          }),
+          width: 100,
+          field: 'tilesEst',
+          suppressMovable: true,
+        },
+        {
+          headerName: intl.formatMessage({
+            id: 'export-table.table-column-header.bbox.text',
+          }),
+          width: 800,
+          field: 'bbox',
+          cellRenderer: (props: ICellRendererParams): string => {
+            const data = props.data as IExportTaskStatus;
+            const bbox = data.bbox;
+            return renderBbox(bbox);
+          },
+          suppressMovable: true,
+        },
+      ]);
+    }, [intl]);
 
-  },[intl]);
+    useEffect(() => {
+      const updateRowData = (exportedPackages: IExportTaskStatus[]): void => {
+        if (gridApi) {
+          const itemsToUpdate = new Array<IExportTaskStatus>();
+          const itemsToAdd = new Array<IExportTaskStatus>();
+          const itemsToRemove = new Array<IExportTaskStatus>();
 
-  useEffect(( )=>{
-    setRowData(exporterStore.exportedPackages);
-  },[exporterStore.exportedPackages]);
+          // UPDATE or REMOVE 
+          gridApi.forEachNodeAfterFilterAndSort((rowNode) => {
+            const data = rowNode.data as IExportTaskStatus;
+            const item = exportedPackages.find(
+              (elem) => elem.taskId === data.taskId
+            );
+            if (item) {
+              if (
+                item.status !== data.status ||
+                item.lastUpdateTime !== data.lastUpdateTime ||
+                item.progress !== data.progress
+              ) {
+                Object.keys(data).forEach((key: string) => { 
+                  // eslint-disable-next-line
+                  (data as any)[key] = (item as any)[key];
+                });
+                itemsToUpdate.push(data);
+              }
+            } else {
+              itemsToRemove.push(data);
+            }
+          });
 
-  useEffect(() => {
-    if(isOpen){
-      void exporterStore.getGeoPackages();
-    }
-  },[isOpen, exporterStore]);
- 
-  return (
-    <Box id="exportTable">
-      <Dialog open={isOpen} preventOutsideDismiss={true}>
-        <DialogTitle>
-          <FormattedMessage id="export-table.dialog.title" />
-        </DialogTitle>
-        <DialogContent>
-          <Box
-            className="ag-theme-alpine"
-            style={{
-              height: '450px',
-              width: '960px',
-            }}
-          >
-            <AgGridReact
-              pagination={true}
-              paginationPageSize={pageSize}
-              columnDefs={colDef}
-              rowData={rowData}
-              overlayNoRowsTemplate={intl.formatMessage({ id: 'export-table.nodata' })}
-              frameworkComponents={{
-                progressRenderer: ProgressRenderer,
-                linkRenderer: LinkRenderer,
+          // ADD not exisitng
+          exportedPackages.forEach((elem) => {
+            let isFound = false;
+            gridApi.forEachNode((rowNode) => {
+              const data = rowNode.data as IExportTaskStatus;
+              isFound = isFound || elem.taskId === data.taskId;
+            });
+            // eslint-disable-next-line
+            if (!isFound) {
+              itemsToAdd.push(elem);
+            }
+          });
+
+          gridApi.applyTransaction({
+            update: itemsToUpdate,
+            remove: itemsToRemove,
+            add: itemsToAdd,
+          });
+        }
+      };
+
+      if (!pollingCycle) setRowData(exporterStore.exportedPackages);
+      else updateRowData(exporterStore.exportedPackages);
+    }, [exporterStore.exportedPackages, pollingCycle, gridApi]);
+
+    useEffect(() => {
+      let pollingInterval: NodeJS.Timeout;
+      if (isOpen) {
+        void exporterStore.getGeoPackages();
+        pollingInterval = setInterval(() => {
+          setPollingCycle(pollingCycle + 1);
+          void exporterStore.getGeoPackages();
+        }, EXPORTER_CONFIG.EXPORT.POLLING_CYCLE_INTERVAL);
+      }
+
+      return (): void => {
+        clearInterval(pollingInterval);
+      };
+    }, [isOpen, exporterStore, pollingCycle]);
+
+    return (
+      <Box id="exportTable">
+        <Dialog open={isOpen} preventOutsideDismiss={true}>
+          <DialogTitle>
+            <FormattedMessage id="export-table.dialog.title" />
+          </DialogTitle>
+          <DialogContent>
+            <Box
+              className="ag-theme-alpine"
+              style={{
+                height: '450px',
+                width: '960px',
               }}
-              />
-          </Box> 
-          <Box style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '16px', gap: '16px' }}>
-            <Button 
-              raised 
-              onClick={handleClose}
             >
-              <FormattedMessage id="general.ok-btn.text" />
-            </Button>
-          </Box>
-        </DialogContent>
-      </Dialog>
-    </Box>
-  );
-});
+              <AgGridReact
+                onGridReady={onGridReady}
+                pagination={true}
+                paginationPageSize={pageSize}
+                columnDefs={colDef}
+                rowData={rowData}
+                overlayNoRowsTemplate={intl.formatMessage({
+                  id: 'export-table.nodata',
+                })}
+                frameworkComponents={{
+                  progressRenderer: ProgressRenderer,
+                  linkRenderer: LinkRenderer,
+                }}
+              />
+            </Box>
+            <Box
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                marginTop: '16px',
+                gap: '16px',
+              }}
+            >
+              <Button raised onClick={handleClose}>
+                <FormattedMessage id="general.ok-btn.text" />
+              </Button>
+            </Box>
+          </DialogContent>
+        </Dialog>
+      </Box>
+    );
+  }
+);
--- src/exporter/models/exportTaskStatus.ts
+++ src/exporter/models/exportTaskStatus.ts
@@ -1,5 +1,12 @@
 import { types, Instance } from 'mobx-state-tree';
 
+export enum ExportStatus {
+  PENDING = 'pending',
+  IN_PROGRESS = 'In-Progress',
+  COMPLETED = 'Completed',
+  FAILED = 'Failed',
+}
+
 const bbox = types.model({
   topRight: types.model({
     lat: types.number,
@@ -15,12 +22,16 @@ export const exportTaskStatus = types.model({
   fileName: types.string,
   sizeEst: types.number,
   tilesEst: types.number,
-  status: types.string,
+  status: types.enumeration<ExportStatus>(
+    'status',
+    Object.values(ExportStatus)
+  ),
   bbox: bbox,
   link: types.string,
   creationDate: types.Date,
   lastUpdateTime: types.Date,
   progress: types.number,
+  taskId: types.string,
 });
 
 export interface IExportTaskStatus extends Instance<typeof exportTaskStatus> {}
