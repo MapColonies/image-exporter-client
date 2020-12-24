17527f2199b6cb9599b25567e93082c40feb22d2
alebinson <newdarkvirus@gmail.com>
2020-12-24 21:46:20 +0200

    fix(eslint): conf typings and cleanup

--- src/exporter/views/exporter-view.tsx
+++ src/exporter/views/exporter-view.tsx
@@ -21,57 +21,8 @@ import { ResponseState } from '../../common/models/ResponseState';
 import { ExportSatusTableDialog } from '../components/export-table/export-table-dialog';
 import { ExportStoreError } from '../../common/models/exportStoreError';
 import { BBoxAreaLimit } from '../../common/helpers/bbox-area';
-import WMTSTileGrid from 'ol/tilegrid/WMTS';
-
-import { get as getProjection } from 'ol/proj';
-import { getWidth, getTopLeft } from 'ol/extent';
-
 
 type ServerType = 'geoserver' | 'carmentaserver' | 'mapserver' | 'qgis';
-
-// const RESOLUTIONS = 20,
-//   TILE_GRANULARITY = 256,
-//   WMTS_RESOLUTION_BASIS = 2,
-//   LAYER_HEIGHT_WIDHT_RATIO = 2;
-
-// const projection = getProjection('EPSG:4326');
-// const projectionExtent = projection.getExtent();
-// let resolutions =[];// new Array<number>(RESOLUTIONS);
-// let matrixIds = new Array<string>(RESOLUTIONS);
-// const size = getWidth(projectionExtent) / TILE_GRANULARITY;
-// for (let z = 0; z < RESOLUTIONS; ++z) {
-//   // generate resolutions and matrixIds arrays for this WMTS
-//   resolutions[z] = (size / Math.pow(WMTS_RESOLUTION_BASIS, z))/LAYER_HEIGHT_WIDHT_RATIO;
-//   matrixIds[z] = z.toString();
-// }
-
-
-//   //  resolutions = [0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
-//   //       0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125, 0.001373291015625,
-//   //       0.0006866455078125, 0.00034332275390625, 0.000171661376953125, 8.58306884765625e-05,
-//   //       4.291534423828125e-05, 2.1457672119140625e-05, 1.0728836059570312e-05,
-//   //       5.364418029785156e-06, 2.682209014892578e-06, 1.341104507446289e-06,
-//   //       6.705522537231445e-07, 3.3527612686157227e-07];
-//   //  matrixIds = ['0','1','2','3','4','5','6','7','8','9',
-//   //                   '10','11','12','13','14','15','16','17','18','19','20', '21'];
-// const wmtsOptions = {
-//   tileGrid:  new WMTSTileGrid({
-//     resolutions: resolutions,
-//       matrixIds: matrixIds,
-//       origin: getTopLeft(projectionExtent),
-//       // tileSize: 256
-//     }),
-//   // name: "WMTS combined_layers",
-//   url: 'http://10.28.11.95:8080/wmts/{Layer}/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
-//   layer: 'combined_layers', 
-//   matrixSet: 'gridname',
-//   format: 'png',
-//   // isBaseLayer: true,
-//   style: 'default',
-//   requestEncoding: 'REST',
-// };
-
-
 
 const wmtsOptions = getWMTSOptions({
   attributions: EXPORTER_CONFIG.WMTS_LAYER.ATTRIBUTIONS,
@@ -82,29 +33,6 @@ const wmtsOptions = getWMTSOptions({
   matrixSet: EXPORTER_CONFIG.WMTS_LAYER.MATRIX_SET,
   style: EXPORTER_CONFIG.WMTS_LAYER.STYLE,
 });
-
-// const wmtsOptions = {
-//     ...wmtsOptionsBasis,
-//     // tileGrid:  new WMTSTileGrid({
-//     //   resolutions: [0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
-//     //     0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125, 0.001373291015625,
-//     //     0.0006866455078125, 0.00034332275390625, 0.000171661376953125, 8.58306884765625e-05,
-//     //     4.291534423828125e-05, 2.1457672119140625e-05, 1.0728836059570312e-05,
-//     //     5.364418029785156e-06, 2.682209014892578e-06, 1.341104507446289e-06,
-//     //     6.705522537231445e-07, 3.3527612686157227e-07],
-//     //     matrixIds: ['0','1','2','3','4','5','6','7','8','9',
-//     //                 '10','11','12','13','14','15','16','17','18','19','20', '21'],
-//     //     tileSize: [256, 256]    
-//     //     }),
-//     name: "WMTS combined_layers",
-//     url: 'http://10.28.11.95:8080/wmts/combined_layers/{TileMatrixSet}/{TileMatrix}/{TileCol}/{TileRow}.png',
-//     layer: 'combined_layers',
-//     matrixSet: 'gridname',
-//     format: 'png',
-//     isBaseLayer: true,
-//     style: 'default',
-//     requestEncoding: 'REST',
-// };
 
 const wmsOptions = getWMSOptions({
   attributions: EXPORTER_CONFIG.WMS_LAYER.ATTRIBUTIONS,
