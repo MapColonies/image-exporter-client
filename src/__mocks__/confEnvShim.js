45d1263138de9f7a9c8170d7759d77d4c2c5580d
alebinson <newdarkvirus@gmail.com>
2020-12-24 22:15:34 +0200

    fix(unit-test): fixed

--- src/__mocks__/confEnvShim.js
+++ src/__mocks__/confEnvShim.js
@@ -1,6 +1,7 @@
 if (!window._env_) {
   window._env_ = (function (undefined) {
     return {
+      LANGUAGE: 'en',
       MAP_SERVER: 'MAP_SERVER',
       PUBLISH_POINT: 'PUBLISH_POINT',
       CHANNEL: 1002,
@@ -11,6 +12,21 @@ if (!window._env_) {
       ACTIVE_LAYER: 'ACTIVE_LAYER',
       BBOX: {
         limit: 20000,
+      },
+      EXPORT: {
+        defaultZoomLevel: 18,
+        maxZoomLevel: 22
+      },
+      MAP: {
+        center: [35.14, 31.39],
+        zoom: 8,
+      },
+      ACTIVE_LAYER_PARAMS: {
+        urlPattern : 'URL_PATTERN',
+        matrixSet: 'MATRIX_SET',
+        style: 'STYLE',
+        projection: 'PROJECTTION',
+        format: 'FORMAT'
       },
     };
   })(void 0);
