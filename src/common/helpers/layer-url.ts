4c4d9d9d97ba97088a99f75ff67c32a3637b7eac
alebinson <newdarkvirus@gmail.com>
2020-12-28 12:25:15 +0200

    refactor(config): active-layer-properties

--- src/common/helpers/layer-url.ts
+++ src/common/helpers/layer-url.ts
@@ -8,17 +8,17 @@ const getParamsString = (params: {[key: string]: string | boolean | number}) : s
 
 const fullUrls : {[key : string] : string} = {
   // TODO: 'WMS_LAYER': `${EXPORTER_CONFIG.WMS_LAYER.URL}?${getParamsString(EXPORTER_CONFIG.WMS_LAYER.PARAMS)}`,
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  'WMS_LAYER': `${EXPORTER_CONFIG.EXPORT.EXPORT_URL}`,
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  'WMTS_LAYER': EXPORTER_CONFIG.WMTS_LAYER.URL,
+  // eslint-disable-next-line
+  'WMS_LAYER': EXPORTER_CONFIG.ACTIVE_LAYER_PROPERTIES.exportUrlPattern,
+  // eslint-disable-next-line
+  'WMTS_LAYER': EXPORTER_CONFIG.ACTIVE_LAYER_PROPERTIES.exportUrlPattern,
   // eslint-disable-next-line @typescript-eslint/naming-convention
   'XYZ_LAYER': EXPORTER_CONFIG.XYZ_LAYER.URL,
   // eslint-disable-next-line @typescript-eslint/naming-convention
   'OSM_DEFAULT': 'NOT_VALID_URL'
 };
 
-export const getLayerUrl = () : string => {
+export const getExportLayerUrl = () : string => {
   const activeLayer = EXPORTER_CONFIG.ACTIVE_LAYER as string;
   return fullUrls[activeLayer];
 }
