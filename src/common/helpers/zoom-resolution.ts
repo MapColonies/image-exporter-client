8fca0028d9ca5197114011f8a2e8a6b530734b2f
alebinson <newdarkvirus@gmail.com>
2021-01-13 10:04:41 +0200

    fix(zoom-resolution): calculation instead of lookup

--- src/common/helpers/zoom-resolution.ts
+++ src/common/helpers/zoom-resolution.ts
@@ -1,9 +1,20 @@
 import EXPORTER_CONFIG from '../../common/config';
 
-const getResolutionInMeteres = (zoomLevel: number): number | string => {
-  const zoom = EXPORTER_CONFIG.MAP.LOOKUPS.LEVEL_RESOLUTION.find(zoomObject => zoomObject.zoom === zoomLevel);
+// const prettyFloat = (value: number, precision: number=0, localize: boolean=false): string => {
+//   const rounded = (!isNaN(precision) && parseInt(precision.toString(), 10) > 0)
+//       ? parseFloat(value.toString()).toFixed(parseInt(precision.toString(), 10))
+//       : value;
+//   const trimmed = parseFloat(rounded.toString()).toString();
+//   if (localize) {
+//       return parseFloat(trimmed).toLocaleString();
+//   }
+//   return trimmed;
+// };
 
-  return zoom?.pixel_size_meters ?? 'UNMAPPED';
+const getResolutionInMeteres = (zoomLevel: number): number | string => {
+  // prettyFloat() might be used to trim trailing zeros
+  // eslint-disable-next-line
+  return (EXPORTER_CONFIG.MAP.ZERO_ZOOM_LEVEL_RESOLUTION / Math.pow(2, zoomLevel)).toFixed(3);
 };
 
 export { getResolutionInMeteres };
