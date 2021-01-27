965da66a2eec1fbed6caa722fbd3a0320b9e64e5
alebinson <newdarkvirus@gmail.com>
2021-01-27 18:49:00 +0200

    fix(clien_info): managed in his own module

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserName, fullBrowserVersion, osName, osVersion, deviceType, mobileVendor, mobileModel } from 'react-device-detect';
 import 'mobx-react-lite/batchingForReactDom';
 import Axios, { Method } from 'axios';
 import moment from 'moment';
@@ -8,6 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { StoreProvider, rootStore } from './exporter/models/rootStore';
 import { ExporterResponse } from './exporter/models/exporterStore';
+import { CLIENT_SYSTEM_INFO } from './common/helpers/client-info';
 import EXPORTER_CONFIG from './common/config';
 import logger from './logger/logger';
 import './index.css';
@@ -16,19 +16,6 @@ const store = rootStore.create(
   {},
   {
     fetch: async (url: string, method: Method, params: Record<string, unknown>) => {
-      const { userAgent } = navigator as NavigatorID;
-      /* eslint-disable */
-      const systemInfo = {
-        browser: browserName,
-        browser_version: fullBrowserVersion,
-        os: osName,
-        os_version: osVersion,
-        device: deviceType,
-        mobile_vendor: mobileVendor,
-        mobile_model: mobileModel,
-        user_agent: userAgent,
-      };
-      /* eslint-enable */
       const errorMsg = 'CLIENT HTTP ERROR BY AXIOS';
       return Axios.request({
         url, 
@@ -39,7 +26,7 @@ const store = rootStore.create(
       .then((res) => res.data as ExporterResponse)
       .catch ((error) => {
         // eslint-disable-next-line
-        logger.error(errorMsg, {response:error, ...systemInfo, dateTime: moment(new Date()).format('DD/MM/YYYY HH:mm')});
+        logger.error(errorMsg, {response:error, ...CLIENT_SYSTEM_INFO, dateTime: moment(new Date()).format('DD/MM/YYYY HH:mm')});
         throw(error);
       })
     },
